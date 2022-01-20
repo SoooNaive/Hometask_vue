ee6a3f5c34cdc71f6b058deda4df53ae240dc303
Naive <sooo.naive@yandex.ru>
2022-01-20 15:35:14 +0300

    5

---
+++ 5/task/src/components/store-wrapper.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="store-wrapper">
+      <router-view></router-view>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'store-wrapper',
+        data() {
+             return {}
+        }
+     }
+</script>
+
+
+<style>
+  .store-wrapper {
+    margin: 0 auto;
+  }
+</style>
